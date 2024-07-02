6fba90eec4af4f632f0237d53bd5fd2c160024b5
Redi <167893131+4redi@users.noreply.github.com>
2024-07-02 06:32:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random as rand
+import pandas as pd
+
+dataset=pd.read_csv(r'dataset/AnimeQuotes.csv')
+def generate_quote():
+  random_quote=dataset.sample(n=1).iloc[0]
+  print(f"{random_quote['Quote']} \n\tsaid by {random_quote['Character']} from {random_quote['Anime']}  ")
+generate_quote()
+  
