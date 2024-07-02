50552b24a662007da1fe234b52b3783121b69050
Redi <167893131+4redi@users.noreply.github.com>
2024-07-02 06:37:20 +0000

    Added comment

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import random as rand
 import pandas as pd
 
-dataset=pd.read_csv(r'dataset/AnimeQuotes.csv')
+dataset = pd.read_csv(r'dataset/AnimeQuotes.csv')
+
+
 def generate_quote():
-  random_quote=dataset.sample(n=1).iloc[0]
-  print(f"{random_quote['Quote']} \n\tsaid by {random_quote['Character']} from {random_quote['Anime']}  ")
+  random_quote = dataset.sample(n=1).iloc[0]
+  print(
+      f"{random_quote['Quote']} \n\tsaid by {random_quote['Character']} from {random_quote['Anime']}  "
+  )
+
+
 generate_quote()
-  
+
+# focus on making it an interface instead of terminal
