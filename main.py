51699a1500a04bf76f3ea41e77980b8a9fcd36f6
Redi <redicurrii@yahoo.com>
2024-07-02 10:13:41 +0200

    Installed flask and created web

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from flask import Flask,render_template
 import random as rand
 import pandas as pd
+
+app=Flask(__name__)
 
 dataset = pd.read_csv('dataset/AnimeQuotes.csv', encoding='utf-8')
 
 
 def generate_quote():
   random_quote = dataset.sample(n=1).iloc[0]
-  print(
-      f"{random_quote['Quote']} \n\tsaid by {random_quote['Character']} from {random_quote['Anime']}  "
-  )
+  return {
+    'quote':random_quote['Quote'],
+    'character':random_quote['Character'],
+    'anime':random_quote['Anime']
+  }
 
+@app.route('/')
+def show():
+  quote=generate_quote()
+  return render_template('index.html',quote=quote) #one is the variable in python file the other on the .html file
 
-generate_quote()
-
-# focus on making it an interface instead of terminal
+if __name__=='__main__':
+  app.run(debug=True)
